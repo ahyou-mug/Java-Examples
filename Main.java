78b1e85c966a8c1edc74e73ae606344d6ed50820
no0dles <62745441+ahyou-mug@users.noreply.github.com>
2022-07-05 15:32:11 +1200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void start(String[] args) {
+		// TODO Auto-generated method stub
+		Arrays j = new Arrays();
+		Scanner scanner = new Scanner(System.in);
+		
+		
+		for(int i=0; i<5; i++) {
+			if(j.animals[i] != null) {
+				//do nothing
+			} else {
+				System.out.println("Please enter an animal: ");
+				j.animals[i] = scanner.next();
+			}
+		}
+		
+		for(int i=0; i<5; i++) {
+			System.out.println(j.animals[i]);
+		}
+		scanner.close();
+				
+	}
+		
+	public static void changeIt(String[] args) {
+		Arrays j = new Arrays();
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter the index of the animal array you wish to change: ");
+		int x = (scanner.nextInt()) - 1;
+		System.out.println("Enter the new animal: ");
+		String y = scanner.next();
+		j.animals[x] = y;
+		scanner.close();
+	}
+	
+}
+
+	
+
